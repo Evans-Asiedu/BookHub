7ca124693aaa218c1363f214a0aec4aa908156be
Evans-Asiedu <evansasiedu352@gmail.com>
2024-08-22 10:05:17 +0000

    Build the list group component

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import Pagination from "./common/pagination";
+import ListGroup from "./common/listGroup";
 import { getBooks } from "./../services/fakeBookService";
+import { genres, getGenres } from "../services/genreServices";
 import { paginate } from "../utils/paginate";
 import "../css/styles.css";
 
 class Books extends Component {
   state = {
-    books: getBooks(),
+    books: [],
+    genres: [],
     pageSize: 3,
     currentPage: 1,
   };
 
+  componentDidMount() {
+    this.setState({ books: getBooks(), genres: getGenres() });
+  }
+
   handlePageChange = (page) => {
     this.setState({ currentPage: page });
+  };
+
+  handleGenreSelect = (genre) => {
+    console.log(genre);
   };
 
   render() {
@@ -24,58 +35,66 @@ class Books extends Component {
     const books = paginate(allBooks, currentPage, pageSize);
 
     return (
-      <React.Fragment>
-        <p>There are {count} movies in the database.</p>
-        <div className="container mt-4">
-          <div className="row">
-            {books.map((book) => (
-              <div className="col-md-4 mb-4" key={book.id}>
-                <div className="card rounded-lg" style={{ width: "18rem" }}>
-                  <a
-                    href={book.volumeInfo.previewLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      className="card-img-top"
-                      style={{ height: "22rem" }}
-                      src={book.volumeInfo.imageLinks.smallThumbnail}
-                      alt=""
-                    />
-                  </a>
-                  <div className="card-body">
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={this.state.genres}
+            onItemSelect={this.handleGenreSelect}
+          />
+        </div>
+        <div className="col">
+          <p>There are {count} movies in the database.</p>
+          <div className="container mt-4">
+            <div className="row">
+              {books.map((book) => (
+                <div className="col-md-4 mb-4" key={book.id}>
+                  <div className="card rounded-lg" style={{ width: "16rem" }}>
                     <a
                       href={book.volumeInfo.previewLink}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <h5 className="card-title d-block text-truncate">
-                        {book.volumeInfo.title}
-                      </h5>
+                      <img
+                        className="card-img-top"
+                        style={{ height: "22rem" }}
+                        src={book.volumeInfo.imageLinks.smallThumbnail}
+                        alt=""
+                      />
                     </a>
-                    <p className="card-text d-block text-truncate">
-                      <span>{book.volumeInfo.authors[0]}</span>
-                      {book.volumeInfo.authors[1] && (
-                        <span> & {book.volumeInfo.authors[1]}</span>
-                      )}
-                    </p>
-                    <footer className="text-muted d-flex justify-content-between">
-                      <span>{book.volumeInfo.categories[0]}</span>
-                      <span>{book.volumeInfo.pageCount} pages</span>
-                    </footer>
+                    <div className="card-body">
+                      <a
+                        href={book.volumeInfo.previewLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <h5 className="card-title d-block text-truncate">
+                          {book.volumeInfo.title}
+                        </h5>
+                      </a>
+                      <p className="card-text d-block text-truncate">
+                        <span>{book.volumeInfo.authors[0]}</span>
+                        {book.volumeInfo.authors[1] && (
+                          <span> & {book.volumeInfo.authors[1]}</span>
+                        )}
+                      </p>
+                      <footer className="text-muted d-flex justify-content-between">
+                        <span>{book.volumeInfo.categories[0]}</span>
+                        <span>{book.volumeInfo.pageCount} pages</span>
+                      </footer>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
         </div>
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
-      </React.Fragment>
+      </div>
     );
   }
 }
---
+++ src/services/genreServices.js
@@ -0,0 +1,17 @@
+export const genres = [
+  { id: 0, name: "All" },
+  { id: 1, name: "Fiction" },
+  { id: 2, name: "Non-fiction" },
+  { id: 3, name: "Mystery" },
+  { id: 4, name: "Romance" },
+  { id: 5, name: "Science" },
+  { id: 6, name: "Fantasy" },
+  { id: 7, name: "Horror" },
+  { id: 8, name: "History" },
+  { id: 9, name: "Self-help" },
+  { id: 10, name: "Biography" },
+];
+
+export function getGenres() {
+  return genres;
+}
