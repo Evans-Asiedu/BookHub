4041e21b078c6f5482efa1afab5ed8776b3a0c02
Evans-Asiedu <evansasiedu352@gmail.com>
2024-08-24 11:19:49 +0000

    Update the nav bar and body background color

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/styles.css";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-color">
       <Link className="container ml-lg-5 navbar-brand " to="/">
         BookHub
       </Link>
